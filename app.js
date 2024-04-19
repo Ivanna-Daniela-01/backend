9658c55e84bcb1ab18be64a59a6296842a0f288d
Ivanna Cevallos <ivanna.cevallos@epn.edu.ec>
2024-04-19 12:13:19 -0500

    add personroutes  and app.use

--- app.js
+++ app.js
@@ -5,6 +5,20 @@ const bodyParser = require('body-parser');
 var app = express();
 var sessions=require('express-session');
 var cookieParser=require('cookie-parser');
+var personRoutes=require('./routes/person.route');
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
+
+
+app.use((req,res,next)=>{
+    res.header('Access-Control-Allow-Origin','*');
+    res.header('Access-Control-Allow-Headers','Authorization, X-API-KEY, X-Request-With, Content-Type,Accept, Access-Control-Allow, Request-Method')
+    res.header('Access-Control-Allow-Methods','GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow','GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Access-Control-Allow-Credentials',true);
+    next();
+});
+
 //la creaci[on dura 24 horas en milisegundos]
 const oneDay=1000*60*60*24;
 //agregar a nuestro app el uso de sesiones
@@ -18,4 +32,5 @@ app.use(sessions({
     resave:false
 }));
 app.use(cookieParser());
+app.use('/',personRoutes);
 module.exports=app;
