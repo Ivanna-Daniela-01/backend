3ccb9a3a36bedcf9fd7784a0200ac8056c784160
Ivanna Cevallos <ivanna.cevallos@epn.edu.ec>
2024-04-19 13:56:41 -0500

    CREATION DELETEPERSON CONTROLLER AND ROUTE

--- controllers/person.controller.js
+++ controllers/person.controller.js
@@ -13,20 +13,35 @@ const personController = {
   async savePerson(req, res) {
     const { name, lastname, mail, password } = req.body;
 
-    // Execute SQL query to insert the person's data into the database
-    const query = 'INSERT INTO person (name, lastname, mail, password) VALUES ($1, $2, $3, $4)';
-    const values = [name, lastname, mail, password];
-  
-    pool.query(query, values)
-      .then(() => {
-        // Send success response
-        res.status(200).json({ message: 'Person saved successfully', requestBody: req.body });
+    // Check if the email already exists in the database
+    pool.query('SELECT * FROM person WHERE mail = $1', [mail])
+      .then(result => {
+        if (result.rows.length > 0) {
+          // If email already exists, send a message indicating that the user is already in the database
+          res.status(400).json({ message: 'User with this email already exists' });
+        } else {
+          // If email doesn't exist, execute the query to insert the person's data into the database
+          const query = 'INSERT INTO person (name, lastname, mail, password) VALUES ($1, $2, $3, $4)';
+          const values = [name, lastname, mail, password];
+      
+          pool.query(query, values)
+            .then(() => {
+              // Send success response
+              res.status(200).json({ message: 'Person saved successfully', requestBody: req.body });
+            })
+            .catch(error => {
+              console.error('Error saving person:', error);
+              // Send error response
+              res.status(500).json({ message: 'Error saving person' });
+            });
+        }
       })
       .catch(error => {
-        console.error('Error saving person:', error);
+        console.error('Error checking email existence:', error);
         // Send error response
-        res.status(500).json({ message: 'Error saving person' });
+        res.status(500).json({ message: 'Error checking email existence' });
       });
+    
   },
 
   // Function to fetch all persons from the database
@@ -42,7 +57,27 @@ const personController = {
       // Send error response
       res.status(500).json({ message: 'Error fetching person' });
     }
+  },
+
+   // Function to delete an specific person from the database
+  async deletePerson(req, res){
+    try {
+        const personId = req.params.id;
+    
+        // Execute SQL query to delete the person by ID
+        const query = 'DELETE FROM person WHERE id = $1';
+        const values = [personId];
+        await pool.query(query, values);
+    
+        // Send success response
+        res.status(200).json({ message: 'Person deleted successfully', deletedId: personId });
+      } catch (error) {
+        console.error('Error deleting person:', error);
+        // Send error response
+        res.status(500).json({ message: 'Error deleting person' });
+      }
   }
+  
 };
 
 module.exports = personController;
--- routes/person.route.js
+++ routes/person.route.js
@@ -7,4 +7,5 @@ const personController = require('../controllers/person.controller');
 // Route to save a new person
 router.post('/savePerson', personController.savePerson);
 router.get('/getPerson', personController.getPerson);
+router.delete('/deletePerson/:id', personController.deletePerson);
 module.exports = router;
