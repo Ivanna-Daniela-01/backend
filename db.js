ed92f74e7b0173891ea94a8091d06811d52cb945
Ivanna Cevallos <ivanna.cevallos@epn.edu.ec>
2024-05-13 12:10:45 -0500

    MOVE CREDENTIALS TO .env FILE

--- db.js
+++ db.js
@@ -1,13 +1,16 @@
 // db.js
 const { Pool } = require('pg');
+// app.js or server.js
+require('dotenv').config();
+
 
 // Create a new Pool instance to handle PostgreSQL connections
 const pool = new Pool({
-  user: 'postgres',
-  host: 'localhost',
-  database: 'Users',
-  password: 'abc123',
-  port: 5432, // Default PostgreSQL port
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_DATABASE,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
 });
 
 module.exports = pool;
